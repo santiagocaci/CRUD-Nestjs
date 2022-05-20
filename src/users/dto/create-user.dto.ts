e34cfcf08da89dbbacd1868dce24f1f90d7eb746
Santiago Caci <santiago.caci@gmail.com>
2022-05-20 19:15:16 -0300

    Fixed PostUser not space

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,18 +1,22 @@
-import { IsEmail, IsNotEmpty, MaxLength, MinLength } from "class-validator";
+import { Transform } from "class-transformer";
+import { IsEmail, IsLowercase, IsNotEmpty, MaxLength, MinLength, NotContains } from "class-validator";
 
 export class CreateUserDto {
 
   @IsNotEmpty()
   @IsEmail()
+  @Transform(({ value }) => value.toLowerCase())
   email: string;
 
   @IsNotEmpty()
   @MaxLength(12)
   @MinLength(6)
+  @NotContains(' ', { message: 'the password must not have spaces' })
   password: string;
 
   @IsNotEmpty()
   @MinLength(4)
+  @NotContains(' ', { message: 'the username must not have spaces' })
   username: string;
 
 }
